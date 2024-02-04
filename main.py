b94287c79a9c0568f8781447e3109ce54a49b4be
emagnubcp <emagnubcp@gmail.com>
2024-02-04 22:49:19 +0000

    https://www.youtube.com/watch?v=dlfn1WrZZ2M

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+name: str = 'Lele'
+value: int = 10
+
+
+def hello():
+    return 'Hello '
+
+def my_square():
+    return n_** 2
+
+print(f'Hello {hello()}!')
+print('Here is a calculation 20 + 40 = ', str(20+40))
+print(f'Here is a calculation 20 + 40 = {20+40}')
+print(f'Here is a calculation {20 + 40 = }')
+
+print(f'Here is some data: {value = }')
+
+print(f'This function : {value = }')
+
+
+name: str = 'Manny'
+age: int = 25
+weight:float = 55.5
+
+print(f'MY name is: {name} and my age is: {age} and i weight {weight}')
